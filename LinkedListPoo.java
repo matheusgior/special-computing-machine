a31b57e5d525a0077265e057824d9c64c07cf207
matheusgior <matheuszgiordani@gmail.com>
2025-10-06 21:54:37 +0000

    Código inicial

---
+++ LinkedListPoo.java
@@ -0,0 +1,62 @@
+import java.util.LinkedList;
+
+public class LinkedListPoo {
+
+    public static void main(String[] args) {
+        // Criação da LinkedList
+        LinkedList<String> lista = new LinkedList<>();
+        System.out.println("Lista inicial: " + lista);
+
+        // Operações de Inserção
+        System.out.println("\nInserção");
+        lista.addFirst("Elemento A");
+        System.out.println("Após addFirst" + lista);
+
+        lista.addLast("Elemento C");
+        System.out.println("Após addLast"+ lista);
+
+        lista.add(1, "Elemento B");
+        System.out.println("Após add" + lista);
+
+        lista.add("Elemento D");
+        System.out.println("Após add" + lista);
+
+        // Operação de percorrer
+        System.out.println("\nPercorrer");
+        System.out.print("Percorrendo a lista: ");
+        for (String item : lista) {
+            System.out.print(item + " ");
+        }
+        System.out.println();
+
+        // Operação de Pesquisa
+        System.out.println("\nPesquisa");
+        String elementoProcurado = "Elemento B";
+        int indice = lista.indexOf(elementoProcurado);
+        if (indice != -1) {
+            System.out.println("\"" + elementoProcurado + "\" encontrado no índice: " + indice);
+            System.out.println("Elemento no índice 0: " + lista.get(0));
+        } else {
+            System.out.println("\"" + elementoProcurado + "\" não encontrado.");
+        }
+
+        // Operações de Remoção
+        System.out.println("\nRemoção");
+        lista.removeFirst();
+        System.out.println("Após removeFirst(): " + lista);
+
+        lista.removeLast();
+        System.out.println("Após removeLast(): " + lista);
+
+        lista.remove("Elemento B");
+        System.out.println("Após remove(\"Elemento B\"): " + lista);
+
+        if (!lista.isEmpty()) {
+            lista.remove(0);
+            System.out.println("Após remove(0): " + lista);
+        }
+
+        System.out.println("\nLista final: " + lista);
+    }
+}
+
